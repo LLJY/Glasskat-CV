44f60770edd808f409ef799d35295140b8a1c494
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2020-12-05 04:49:44 +0800

    Implement a few more features (Read desc)
    ringbuffer.py: Lazy implementation of a ringbuffer to store frames and save them.
    
    save footage to file system and upload them to firebase storage.
    
    TODO: round and stream the coordinates to the backend.

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import numpy as np
+import grpc
 import argparse
 import cv2
 import time
 import math
 import json
+import firebase_admin
+from firebase_admin import storage, credentials
+import threading
+from ringbuffer import RingBuffer
+from datetime import datetime
+import protos.gk_pb2
+import protos.gk_pb2_grpc
 
-MAX_WIDTH = 800
-MAX_HEIGHT = 448
 parser = argparse.ArgumentParser(description="Classify Cat using image from webcam with mobilenet SSD")
 parser.add_argument("--prototxt", default="MobileNetSSD_deploy.prototxt")
 parser.add_argument("--weights", default="MobileNetSSD_deploy.caffemodel",
@@ -16,6 +22,9 @@ parser.add_argument("--cameraConf", default="c920conf", type=str, help="simple p
                                                                        "configuration for your webcam")
 parser.add_argument("--cuda", default=True, type=bool, help="use cuda or not")
 parser.add_argument("--model")
+parser.add_argument("--width", default=800, help="video width")
+parser.add_argument("--height", default=448, help="video height")
+parser.add_argument("--fps", default=30, help="video framerate")
 args = parser.parse_args()
 camConfig = __import__(args.cameraConf)
 # got the default labels of the mobilenet SSD model we are using
@@ -27,19 +36,61 @@ classNames = {0: 'background',
               17: 'sheep', 18: 'sofa', 19: 'train', 20: 'tvmonitor'}
 relevantClassNames = {0: 'background',
                       5: 'bottle', 8: 'cat', 15: 'person'}
+is_video_saving = False
+print("[INFO]: Initializing Firebase Admin")
+cred = credentials.Certificate("firebase-credentials.json")
+app = firebase_admin.initialize_app(cred, {'storageBucket': 'mcsv-firebase-service.appspot.com'})
+bucket = storage.bucket("mcsv-firebase-service.appspot.com")
 
+print("[INFO]: Initializing gRPC...")
+channel = grpc.insecure_channel("localhost:5001")
+stub = protos.gk_pb2_grpc.GkCoordinatesStub(channel)
 
 def calculate_x_coords(u):
     max_coords = (zCoords / math.tan(math.radians(camConfig.fov / 2))) * 2
     # calculate the camera coordinates and convert it to world coordinates by dividing by max/2
     # so the center of the frame is now 0.
-    return round((u / MAX_WIDTH) * max_coords, 2) - (max_coords / 2)
+    return round((u / args.width) * max_coords, 2) - (max_coords / 2)
+
+
+def save_video(frame):
+    global is_video_saving
+    is_video_saving = True
+    # only save data to the ring buffer if it is full, this acts as a five minute timeout between videos
+    if (ring_buffer.isFull):
+        print("[INFO] Saving video...")
+        dateStr = datetime.now().isoformat()
+        # passing this over to the upload function to define the filename
+        videoName = dateStr + ".mp4"
+        # to output to the local videos/ folder
+        videoPath = "videos/" + videoName
+        out = cv2.VideoWriter(videoPath, cv2.VideoWriter.fourcc(*'mp4v'), args.fps, (int(args.width), int(args.height)))
+        for idx, frame in enumerate(ring_buffer.data):
+            out.write(ring_buffer.next(idx))
+        out.release()
+        # flush the ring buffer
+        ring_buffer.flush()
+        # run the uploading of video in a separate thread!
+        thread = threading.Thread(target=upload_video, args=(videoName, videoPath))
+        thread.start()
+    else:
+        ring_buffer.append(frame)
+    is_video_saving = False
+
+def upload_video(filename, file_location):
+    blob = bucket.blob(filename)
+    blob.upload_from_filename(file_location)
+    blob.make_public()
+    print("Uploaded video URL: "+ blob.public_url)
 
 
 # load the video from our webcam
-gstr = "v4l2src device=/dev/video0 ! video/x-raw,framerate=30/1,width=" + str(MAX_WIDTH) + ",height=" + str(
-    MAX_HEIGHT) + " ! videoconvert ! appsink"
+print("[INFO]: Setting up camera")
+gstr = "v4l2src device=/dev/video0 ! video/x-raw,framerate=30/1,width=" + str(args.width) + ",height=" + str(
+    args.height) + " ! videoconvert ! appsink"
 cap = cv2.VideoCapture(gstr, cv2.CAP_GSTREAMER)
+# cap = cv2.VideoCapture(0)
+print("[INFO]: Setting up Deep Learning Model")
 net = cv2.dnn.readNetFromCaffe(args.prototxt, args.weights)
 if (args.cuda):
     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
@@ -47,13 +98,23 @@ if (args.cuda):
 else:
     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT)
     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+print("[INFO] Initializing Ring Buffer")
+# Initialize the new ring buffer to save the last 5 minutes of footage
+ring_buffer = RingBuffer(max_size=300)
+
+# start the image capture
+print("[INFO] Starting capture...")
+
+# add a boolean here to avoid saving the video multiple times!
 while True:
     start = time.time()
     # Capture frame-by-frame
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
     frame_resized = cv2.resize(frame, (300, 300))  # resize frame for prediction
-
+    # get vcap property
+    args.width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
+    args.height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
     # resize our images for mobilenet (300x300) and normalize the input with 127.5
     blob = cv2.dnn.blobFromImage(frame_resized, 0.007843, (300, 300), (127.5, 127.5, 127.5), False)
     net.setInput(blob)
@@ -62,6 +123,7 @@ while True:
     # Size of frame resize (300x300)
     cols = frame_resized.shape[1]
     rows = frame_resized.shape[0]
+    isCapture = False
     # detections returns a numpy array, access the information in the multi dimensional array like this
     for i in range(detections.shape[2]):
         confidence = detections[0, 0, i, 2]  # Confidence of prediction
@@ -86,8 +148,11 @@ while True:
             midXCoords = (xLeftBottom + xRightTop) / 2
             midYCoords = (yLeftBottom + yRightTop) / 2
 
+            # we are separating the logic as we want an accurate FPS readout while keeping all labels
+            # in the final video frame we are saving to our ring buffer
+            isCapture = class_id in relevantClassNames
             # label the rectangle
-            if (class_id in relevantClassNames):
+            if isCapture:
                 # draw a rectangle according to the bounding box that surrounds the object and only draw when
                 # something is detected
                 cv2.rectangle(frame, (xLeftBottom, yLeftBottom), (xRightTop, yRightTop),
@@ -116,5 +181,12 @@ while True:
 
     cv2.namedWindow("frame", cv2.WINDOW_GUI_NORMAL)
     cv2.imshow("frame", frame)
+
+    if isCapture and not is_video_saving:
+        # run asynchronously to avoid stalling the video
+        thread = threading.Thread(target=save_video, args=(frame,))
+        thread.start()
+
     if cv2.waitKey(1) >= 0:  # Break with ESC
+        print("[INFO] Stopping capture!")
         break
---
+++ ringbuffer.py
@@ -0,0 +1,34 @@
+class RingBuffer:
+    def __init__(self, max_size):
+        self.max_Size = max_size
+        self.data = []
+        # set the current index to 0
+        self.cur = 0
+        self.isFull = False
+
+    def append(self, newData):
+        if (self.isFull):
+            self.data[self.cur] = newData
+            # change the current to zero for the next append to maintain the ring buffer
+            if self.cur == self.max_Size:
+                self.cur = 0
+            else:
+                self.cur += 1
+        else:
+            # if the list is not full yet, just append the data
+            self.data.append(newData)
+            self.isFull = (len(self.data) == self.max_Size)
+
+    # Flushes all the data in the ringbuffer
+    def flush(self):
+        self.isFull = False
+        self.data = []
+        # for JVM, trigger GC here to avoid taking up a lot of ram
+
+    # cur is the current
+
+    def next(self, idx):
+        # if the distance from the current index
+        if idx > self.max_Size - self.cur:
+            return self.data[(idx + self.cur) - self.max_Size]
+        return self.data[self.cur + idx]
